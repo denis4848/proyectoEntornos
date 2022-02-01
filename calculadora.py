09939bb3df6511bdc4e29c612778825ccd51b8a1
Denis Andrei Guimpu Stefan <7032@iesluisbraille.es>
2022-02-01 09:05:51 +0100

    commit 2

--- calculadora.py
+++ calculadora.py
@@ -18,6 +18,22 @@ def resta():
     n2=int(input("Dame numero 2"))
     print("la resta de", n1, "y", n2, "es: ", n1-n2)
 
+def multiplicacion():
+    n1=int(input("Dame numero 1"))
+    n2=int(input("Dame numero 2"))
+    print("la multiplicacion de", n1, "y", n2, "es: ", n1*n2)
+
+def division():
+    n1=int(input("Dame numero 1"))
+    n2=int(input("Dame numero 2"))
+    if(n1>n2 & n2!=0):
+        print("la división de", n1, "y", n2, "es: ", n1/n2)
+    if(n2==0): 
+        print("No se puede dividir entre 0")
+    elif(n2>n1 & n1!=0):
+        print("la división de", n1, "y", n2, "es: ", n2/n1)
+    if(n1==0):
+        print("No se puede dividir entre 0")
 #programa///////////////////////////////////////////
 
       
@@ -28,12 +44,18 @@ while (x!=0):
     x=int(input("""
     0.- salir
     1.- suma 
-    2.- resta 
+    2.- resta
+    3.- multiplicacion 
+    4.- division
+    Dame un numero: 
+        """))
 
     if (x==1): #suma
         suma()
     elif(x==2): #resta
         resta()
+    elif(x==3): #multiplicacion
+        multiplicacion()
+    elif(x==4): #division
+       division()
     
-    Dame un numero: 
-        """))
